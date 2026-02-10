d8a25dcd54f4a9eccbb513452c3c2d9d96c998ad
Logann Leriche <lleriche@c2r12p6.42lehavre.fr>
2026-02-10 15:36:47 +0100

    ex1

--- ex1/data_stream.py
+++ ex1/data_stream.py
@@ -8,7 +8,7 @@ class DataStream(ABC):
         self.stream_id = stream_id
 
     @abstractmethod
-    def process_batch(self) -> str:
+    def process_batch(self, data_batch: List[Any]) -> str:
         pass
 
     def filter_data(self, data_batch: List[Any],
@@ -31,4 +31,69 @@ class SensorStream(DataStream):
         super().__init__(stream_id)
 
     def process_batch(self, data_batch: List[Any]) -> str:
-        
+        try:
+            count = len(data_batch)
+            avg = 0
+            count2 = 0
+            for i in data_batch:
+                if i.get("type") == "temperature":
+                    avg += i.get("value")
+                    count2 += 1
+            if count2 > 0:
+                avg = avg / count2
+            else:
+                avg = 0
+        except Exception as e:
+            return str(e)
+        return (f"Sensor analysis: {count} readings processed,"
+                f" avg temp: {avg} °C")
+
+
+class TransactionStream(DataStream):
+    def __init__(self, stream_id):
+        super().__init__(stream_id)
+
+    def process_batch(self, data_batch: List[Any]) -> str:
+        try:
+            count = len(data_batch)
+            sell = 0
+            buy = 0
+            for i in data_batch:
+                if i.get("type") == "buy":
+                    buy = buy + i.get("value")
+                elif i.get("type") == "sell":
+                    sell = sell + i.get("value")
+            flux = buy - sell
+        except Exception as e:
+            return str(e)
+        return (f"Transaction analysis: {count} operations,"
+                f" net flow: +{flux} units")
+
+
+class EventStream(DataStream):
+    def __init__(self, stream_id):
+        super().__init__(stream_id)
+
+    def process_batch(self, data_batch: List[Any]) -> str:
+        try:
+            count = len(data_batch)
+            error = 0
+            for i in data_batch:
+                if i == "error":
+                    error += 1
+        except Exception as e:
+            return str(e)
+        return (f"Event analysis: {count} events,"
+                f" {error} error detected")
+
+
+class StreamProcessor:
+    def __init__(self):
+        self.streams = []
+
+    def add_stream(self, stream: DataStream):
+        self.streams.append(stream)
+
+    def process_all_streams(self, data_batch: List[Any]):
+        for i in self.add_stream:
+            i.process_batch(self, data_batch)
